c4b46a530aa0a131f9e6e16c1a184bd32e0d0630
CIRUS.mike.note <pinskiy.miahil@gmail.com>
2022-08-19 15:53:16 +0300

    наплакали

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<template>
+  <TheHeader />
+  <main></main>
+  <TheFooter />
+</template>
+
+<script setup>
+import TheHeader from "./components/TheHeader.vue";
+</script>
+
+<style scoped></style>
---
+++ src/components/TheHeader.vue
@@ -0,0 +1,26 @@
+<template>
+  <header>
+    <nav class="bg-gray-300 border-gray-200 dark:bg-gray-900">
+      <div
+        class="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl px-4 md:px-6 py-2.5"
+      >
+        <a href="https://flowbite.com" class="flex items-center">
+          <img
+            src="image/logo.svg"
+            class="mr-3 h-6 sm:h-9"
+            alt="Flowbite Logo"
+          />
+        </a>
+        <div class="flex items-center">
+          <a
+            href="#"
+            class="text-sm font-medium text-red-600 dark:text-red-500 hover:underline"
+            >Регистрация</a
+          >
+        </div>
+      </div>
+    </nav>
+  </header>
+</template>
+<script setup></script>
+<style scoped></style>
